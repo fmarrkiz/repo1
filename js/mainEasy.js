a99c87263e2bb6ea8f95dbe28392908fad9472f3
Marie KF <mariekfruchart@outlook.com>
2025-05-20 07:37:12 +0100

    (mainEasy.js)cleaned the code

--- js/mainEasy.js
+++ js/mainEasy.js
@@ -1,35 +1,38 @@
 "use strict";
 
-const _carouselContainer = document.querySelector(".carousel__container")
-const _carouselSlider = document.querySelectorAll(".item")
-const _slides = document.querySelectorAll(".item__image")
-const _nextButton = document.querySelector(".next")
+const carouselContainer = document.querySelector(".carousel__container")
+const carouselSlider = document.querySelectorAll(".item")
+const slides = document.querySelectorAll(".item__image")
+const nextButton = document.querySelector(".next")
 let slideIndex = 0;
 let intervalId = null;
 
-document.addEventListener('DOMContentLoaded',initialiseSlider);
+document.addEventListener('DOMContentLoaded', initialiseSlider);
 
 function initialiseSlider(){
-    if(carousel.length> 0){
-        slides[slidesIndex].classList.add("displaySlide");
-        intervalID = () // something button
+    if(slides.length> 0){
+        slides[slideIndex].classList.add("displaySlide");
+            }
+          }
+    nextButton.addEventListener('click', nextSlide);
     }
 }
 
-function showSlide(slideIndexndex){
-
-    if(index >= slide){
-slideIndex = 0;
-    } 
-
+function showSlide(n){
+if (n >= slides.length){
+    slideIndex = 0;
+} else if (n < 0) {
+    slideIndex = slides.length -1;
+} 
 
     slides.forEach(slide => {
         slide.classList.remove("displaySlide");
     });
-    slides[slideIndex].classList.add("displaySlide");
 
+    slides[slideIndex].classList.add("displaySlide");
 }
+
 function nextSlide(){
     slideIndex++;
-    showSlide(slideIndex)
+    showSlide(slideIndex);
 }
